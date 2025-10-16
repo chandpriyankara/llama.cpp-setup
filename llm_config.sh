0797d0c80baa4118168bdbdcb71d7137e0a7ea24
Chand Priyankara <chandpriyankara@gmail.com>
2025-10-16 13:57:02 +1100

    install and setup steps

--- llm_config.sh
+++ llm_config.sh
@@ -22,10 +22,11 @@ sudo apt-get -y install cuda-toolkit-12-9
 sudo apt-get install -y cuda-drivers
 
 cmake -B build -DGGML_CUDA=ON -DLLAMA_BUILD_SERVER=ON -DGGML_CUDA_FORCE_MMQ=ON -DGGML_CUDA_FORCE_CUBLAS=ON -DCMAKE_CUDA_ARCHITECTURES=89
-cmake --build build --config Release
+cmake --build build -j 16 --config Release
 
 #wget https://huggingface.co/bartowski/DeepSeek-Coder-V2-Instruct-GGUF/resolve/main/DeepSeek-Coder-V2-Instruct-Q4_K_M.gguf/DeepSeek-Coder-V2-Instruct-Q4_K_M-00001-of-00004.gguf
 #wget https://huggingface.co/TheBloke/deepseek-coder-6.7B-instruct-GGUF/resolve/main/deepseek-coder-6.7b-instruct.Q4_K_M.gguf?download=true
 
 #llama-server -fa --mlock  --n-gpu-layers 12 -m codellama-13b-instruct.Q4_K_M.gguf --host 127.0.0.1 --port 11435
-llama-server -fa --mlock  --n-gpu-layers 16 -m deepseek-coder-6.7b-instruct.Q4_K_M.gguf --host 127.0.0.1 --port 11435
+#llama-server -fa --mlock  --n-gpu-layers 16 -m deepseek-coder-6.7b-instruct.Q4_K_M.gguf --host 127.0.0.1 --port 11435
+##See run command dev/llm folder for more predicted version
